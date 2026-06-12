b74f26617db34e6a037f0dea34c1c9d9a356c688
psi314 <104723182+psi314@users.noreply.github.com>
2026-06-12 14:12:06 +0530

    changes : changed font , minor changes in noctalia , animations in mango

---
+++ config/nvim/init.lua
@@ -0,0 +1,6 @@
+vim.opt.number = true          -- Show line numbers
+vim.opt.tabstop = 4            -- 4 spaces for a tab (standard for C++)
+vim.opt.shiftwidth = 4         -- 4 spaces for indenting
+vim.opt.expandtab = true       -- Convert tabs to spaces
+vim.opt.smartindent = true     -- Smart auto-indenting
+vim.g.mapleader = " "          -- Set leader key to spacebar
